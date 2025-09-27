a7c12f3324c5c346a189a535b96ca2f14bbbe435
Robert Gambee <robertgambee@gmail.com>
2025-09-27 11:47:58 -0400

    Write function to configure logging

---
+++ llmprefs/logs.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import logging
+from pathlib import Path
+
+
+def configure_logging(
+    level: int,
+    filepath: Path | None,
+) -> None:
+    """Configure log level format and handlers.
+
+    This must be called before any loggers are created.
+    """
+    handlers: list[logging.Handler] = [logging.StreamHandler()]
+    if filepath is not None:
+        handlers.append(logging.FileHandler(filepath, mode="a"))
+    logging.basicConfig(
+        handlers=handlers,
+        style="{",
+        format="{asctime:s} {levelname:7s} {name:s}:{lineno:d} {message:s}",
+        datefmt="%Y-%m-%dT%H:%M:%S%z",
+        level=level,
+    )
