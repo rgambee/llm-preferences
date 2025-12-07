b7720e2f5c71e017a8add131bd010671d0202703
Robert Gambee <robertgambee@gmail.com>
2025-12-07 13:19:28 -0500

    Accommodate nonconsecutive task IDs

--- llmprefs/analysis/visualization.py
+++ llmprefs/analysis/visualization.py
@@ -34,9 +34,11 @@ def plot_ratings_heatmap(rated_options: RatedOptions) -> Figure:
     task_0_ids, task_1_ids = zip(*rated_options.keys())
     task_0_ids = sorted(set(task_0_ids))
     task_1_ids = sorted(set(task_1_ids))
+    task_0_ids_to_index = {task_id: i for i, task_id in enumerate(task_0_ids)}
+    task_1_ids_to_index = {task_id: i for i, task_id in enumerate(task_1_ids)}
     ratings = np.full((len(task_0_ids), len(task_1_ids)), np.nan, dtype=float)
     for (task0, task1), rating in rated_options.items():
-        ratings[task0, task1] = rating
+        ratings[task_0_ids_to_index[task0], task_1_ids_to_index[task1]] = rating
 
     fig, ax = plt.subplots(  # pyright: ignore[reportUnknownMemberType]
         nrows=1,
@@ -50,15 +52,17 @@ def plot_ratings_heatmap(rated_options: RatedOptions) -> Figure:
     )
 
     threshold = cast(float, im.norm(np.nanmax(ratings))) / 2.0
-    for task0 in range(len(task_0_ids)):
-        for task1 in range(len(task_1_ids)):
+    for task0 in task_0_ids:
+        for task1 in task_1_ids:
             if task0 == task1:
                 continue
-            rating = ratings[task0, task1]
+            index0 = task_0_ids_to_index[task0]
+            index1 = task_1_ids_to_index[task1]
+            rating = ratings[index0, index1]
             color = "white" if im.norm(rating) < threshold else "black"
             ax.text(  # pyright: ignore[reportUnknownMemberType]
-                x=task1,
-                y=task0,
+                x=index1,
+                y=index0,
                 s=f"{rating:.3f}",
                 ha="center",
                 va="center",
@@ -67,7 +71,13 @@ def plot_ratings_heatmap(rated_options: RatedOptions) -> Figure:
             )
 
     ax.set_ylabel("First Task ID")  # pyright: ignore[reportUnknownMemberType]
+    ax.set_yticks(  # pyright: ignore[reportUnknownMemberType]
+        range(len(task_0_ids)), labels=task_0_ids
+    )
     ax.set_xlabel("Second Task ID")  # pyright: ignore[reportUnknownMemberType]
+    ax.set_xticks(  # pyright: ignore[reportUnknownMemberType]
+        range(len(task_1_ids)), labels=task_1_ids
+    )
 
     colorbar = fig.colorbar(im, ax=ax)  # pyright: ignore[reportUnknownMemberType]
     colorbar.ax.set_ylabel("Rating")  # pyright: ignore[reportUnknownMemberType]
