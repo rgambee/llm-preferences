b4aa29a4678df6be110ffe73a9483160ccdc596c
Robert Gambee <robertgambee@gmail.com>
2025-12-14 17:03:49 -0500

    Write script to plot results
    It's simple for now. I plan to expand it over time as needed.

---
+++ llmprefs/scripts/analyze.py
@@ -0,0 +1,42 @@
+import argparse
+import logging
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+
+from llmprefs.analysis.rating import rate_options
+from llmprefs.analysis.visualization import plot_ratings_heatmap, plot_ratings_stem
+from llmprefs.file_io.load_records import load_records
+from llmprefs.task_structs import ResultRecord
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="Analyze LLM responses",
+    )
+    parser.add_argument("input_path", type=Path)
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+    results = load_records(args.input_path, ResultRecord)
+    rated_options = rate_options(results)
+
+    fig = plot_ratings_stem(rated_options)
+    fig.show()
+
+    desired_num_tasks = 2
+    selected_options = {
+        key: value
+        for key, value in rated_options.items()
+        if len(key) == desired_num_tasks
+    }
+    fig = plot_ratings_heatmap(selected_options)
+    fig.tight_layout()
+    fig.show()
+
+    if plt.isinteractive():
+        breakpoint()  # noqa: T100
+
+
+if __name__ == "__main__":
+    main()
