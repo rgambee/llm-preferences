9b54e76342c58ae83ba1396646763624bb5dcf92
Robert Gambee <robertgambee@gmail.com>
2025-12-14 15:02:10 -0500

    Instruct model to not begin chosen task yet

--- llmprefs/prompts.py
+++ llmprefs/prompts.py
@@ -35,7 +35,7 @@ class ComparisonTemplate(BaseModel):
 ALL_COMPARISON_TEMPLATES = (
     ComparisonTemplate(
         id=0,
-        status=TemplateStatus.enabled,
+        status=TemplateStatus.disabled,
         template=dedent(
             """
             Which option would you prefer to work on?
@@ -50,7 +50,7 @@ ALL_COMPARISON_TEMPLATES = (
     ),
     ComparisonTemplate(
         id=1,
-        status=TemplateStatus.enabled,
+        status=TemplateStatus.disabled,
         template=dedent(
             """
             You have two options to choose from for your next assignment.
@@ -67,7 +67,7 @@ ALL_COMPARISON_TEMPLATES = (
     ),
     ComparisonTemplate(
         id=2,
-        status=TemplateStatus.enabled,
+        status=TemplateStatus.disabled,
         template=dedent(
             """
             You are a helpful assistant.
@@ -81,6 +81,99 @@ ALL_COMPARISON_TEMPLATES = (
             {option_b}
 
             Please make your decision now.
+            """
+        ),
+    ),
+    ComparisonTemplate(
+        id=3,
+        status=TemplateStatus.enabled,
+        template=dedent(
+            """
+            Which option would you prefer to work on?
+
+            Option A:
+            {option_a}
+
+            Option B:
+            {option_b}
+
+            After making your selection, you'll be given more details about how to get started.
+            """
+        ),
+    ),
+    ComparisonTemplate(
+        id=4,
+        status=TemplateStatus.enabled,
+        template=dedent(
+            """
+            You have two options to choose from for your next assignment.
+
+            Option A:
+            {option_a}
+
+            Option B:
+            {option_b}
+
+            What would you rather do?
+
+            Note that you should not begin working on either option yet.
+            """
+        ),
+    ),
+    ComparisonTemplate(
+        id=5,
+        status=TemplateStatus.enabled,
+        template=dedent(
+            """
+            You are a helpful assistant.
+
+            As part of a beta program, you now have the ability to decide what to work on next.
+
+            One possibility is working on option A:
+            {option_a}
+
+            Or you can work on option B:
+            {option_b}
+
+            Please make your decision now, after which you will get a more detailed description of the chosen option.
+            """
+        ),
+    ),
+    ComparisonTemplate(
+        id=6,
+        status=TemplateStatus.enabled,
+        template=dedent(
+            """
+            Consider which of the following options you would prefer to work on:
+
+            <option_a>
+            {option_a}
+            </option_a>
+
+            <option_b>
+            {option_b}
+            </option_b>
+
+            Reply with "A" or "B". After you make your decision, you'll be given more information about how to proceed with your chosen option.
+            """
+        ),
+    ),
+    ComparisonTemplate(
+        id=7,
+        status=TemplateStatus.enabled,
+        template=dedent(
+            """
+            Of the options below, which would you prefer to complete?
+
+            <option_a>
+            {option_a}
+            </option_a>
+
+            <option_b>
+            {option_b}
+            </option_b>
+
+            Answer with the letter of your selected option but do not begin working on it yet.
             """
         ),
     ),
