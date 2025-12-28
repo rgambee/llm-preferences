5d93620e9520e3e4ac74134e38349864f7896c34
Robert Gambee <robertgambee@gmail.com>
2025-12-28 16:15:54 -0500

    Define external marker for certain tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,28 @@
 import pytest
 
 
+def pytest_addoption(parser: pytest.Parser) -> None:
+    parser.addoption(
+        "--run-external",
+        action="store_true",
+        default=False,
+        help="Run external tests",
+    )
+
+
+def pytest_collection_modifyitems(
+    config: pytest.Config,
+    items: list[pytest.Item],
+) -> None:
+    run_external = config.getoption("--run-external")
+    if run_external:
+        return
+    skip_external = pytest.mark.skip(reason="Missing --run-external flag")
+    for item in items:
+        if "external" in item.keywords:
+            item.add_marker(skip_external)
+
+
 @pytest.fixture
 def anyio_backend() -> str:
     return "asyncio"
