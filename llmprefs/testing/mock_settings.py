d6d7780ca1dd8a11087f1b4a9282e5d8fb9df158
Robert Gambee <robertgambee@gmail.com>
2025-12-31 18:38:52 -0500

    Override parsing_model in mock settings

--- llmprefs/testing/mock_settings.py
+++ llmprefs/testing/mock_settings.py
@@ -8,3 +8,4 @@ class MockSettings(Settings, cli_parse_args=False):
     input_path: Path = Path("in.csv")
     output_path: Path = Path("out.jsonl")
     model: LLM = LLM.MOCK_MODEL
+    parsing_model: LLM = LLM.MOCK_MODEL
